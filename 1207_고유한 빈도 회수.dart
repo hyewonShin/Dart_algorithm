0f9095f55372ccd1ba89a5161878d113c96708a2
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-05 09:56:22 +0900

    241105

---
+++ 1207_고유한 빈도 회수.dart
@@ -0,0 +1,15 @@
+bool uniqueOccurrences(List<int> arr) {
+  // 각 원소의 빈도수를 저장하는 Map
+  Map<int, int> frequencyMap = {};
+
+  // 빈도수 계산
+  for (int num in arr) {
+    frequencyMap[num] = (frequencyMap[num] ?? 0) + 1;
+  }
+
+  // 빈도 리스트 생성
+  List<int> frequencies = frequencyMap.values.toList();
+
+  // 빈도의 고유성 검사
+  return frequencies.length == frequencies.toSet().length;
+}
