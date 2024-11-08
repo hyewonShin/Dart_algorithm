bbbcdf7ddb2eef87f98f83b94d83da8ebe3280aa
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-08 11:45:37 +0900

    241108

---
+++ 11_물이 가장 많은 용기.dart
@@ -0,0 +1,20 @@
+int maxArea(List<int> height) {
+  int left = 0;
+  int right = height.length - 1;
+  int maxArea = 0;
+
+  while (left < right) {
+    int currentHeight = min(height[left], height[right]);
+    int currentArea = currentHeight * (right - left);
+    maxArea = max(maxArea, currentArea);
+
+    // 낮은 높이의 포인터를 움직여 더 높은 영역을 찾음
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
+}
