0e3eff5cf694ee5017327e719d960dba85ab0c8e
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-21 09:54:38 +0900

    241121

--- 104_이진 트리의 최대.dart
+++ 104_이진 트리의 최대.dart
@@ -1,12 +1,9 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *   int val;
- *   TreeNode? left;
- *   TreeNode? right;
- *   TreeNode([this.val = 0, this.left, this.right]);
- * }
- */
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
 
 int maxDepth(TreeNode? root) {
   if (root == null) {
